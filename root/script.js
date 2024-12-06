ef845f1c962bad6027b4f6200036e4afa98b21b8
Isabelle <isabellebsvieira>
2024-12-05 21:52:39 -0300

    Edit and delete added-marked lib working

--- root/script.js
+++ root/script.js
@@ -1,21 +1,41 @@
-const addBtn = document.getElementById('add')
+const addBtn = document.getElementById('add');
 
-addBtn.addEventListener('click', () => addNewNote() )
+addBtn.addEventListener('click', () => addNewNote(''));
+
 function addNewNote(text = '') {
-    const note = document.createElement('div')
-    note.classList.add('note')
 
-    note.innerHTML= `
+    const note = document.createElement('div');
+    note.classList.add('note');
+
+    note.innerHTML = `
         <div class="tools">
-            <button class="edit"> <i class="fa-solid fa-edit"></i> </button>
-            <button class="delete"> <i class="fa-solid fa-trash-alt"></i> </button>
+            <button class="edit"><i class="fa-solid fa-edit"></i></button>
+            <button class="delete"><i class="fa-solid fa-trash-alt"></i></button>
         </div>
-        
         <div class="main ${text ? "" : "hidden"}"></div>
-            <textarea class="main ${text ? "hidden" : ""}"></textarea>
-    `
+        <textarea class="${text ? "hidden" : ""}"></textarea>`;
 
-    document.body.appendChild(note)
-} 
+    const editBtn = note.querySelector('.edit');
+    const delBtn = note.querySelector('.delete');
+    const main = note.querySelector('.main');
+    const textArea = note.querySelector('textarea');
 
+    textArea.value = text;
+    main.innerHTML = text ? marked.parse(text) : "";
 
+    delBtn.addEventListener('click', () => {
+        note.remove();
+    });
+
+    editBtn.addEventListener('click', () => {
+        main.classList.toggle('hidden');
+        textArea.classList.toggle('hidden');
+    });
+
+    textArea.addEventListener('input', (e) => {
+        const { value } = e.target;
+        main.innerHTML = marked.parse(value);
+    });
+
+    document.body.appendChild(note);
+}
